2d681e8a8e0da5e122047b851b315385107b5d29
masopego <masopego@gmail.com>
2020-08-08 19:50:51 +0200

    Add images

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from '../../images/Rick_and_Morty_-_logo.png';
+import Plane from '../../images/images_nave.png';
 
 class Landing extends React.Component {
   render() {
     return (
       <>
         <div>Holi Caracolii</div>
+        <img src={Logo} alt="Rick and Morty" />
+        <img src={Plane} alt="Airplane" />
         <Link to="/rick-and-morty-characters">Comenzar</Link>
       </>
     );
